29f19c9e2444f4d671038a319762e37f0d724fc8
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-31 13:36:08 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+const ulStyle = {
+  paddingLeft: '30px'
+}
+
 export default function Footer() {
   return (
     <div>
       <div className="icons-attribution">
         <div> <small> Chess Icons And Favicon (extracted) By en:User:Cburnett [<a href="http://www.gnu.org/copyleft/fdl.html">GFDL</a>, <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA-3.0</a>, <a href="http://opensource.org/licenses/bsd-license.php">BSD</a> or <a href="http://www.gnu.org/licenses/gpl.html">GPL</a>], <a href="https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces">via Wikimedia Commons</a> </small></div>
       </div>
-      <ul>
+      <ul style={ulStyle}>
         <li><a href="https://github.com/TalhaAwan/react-chess" target="_blank" rel="noopener noreferrer">Source Code</a> </li>
         <li><a href="https://www.techighness.com/post/develop-two-player-chess-game-with-react-js/">Blog Post</a></li>
       </ul>
